f4f20e4500a3b521ddd7c36c6ec2156f73f6f5ae
nevitones <nevitones@gmail.com>
2017-07-12 00:04:51 -0300

    Add z attribute to the map layer

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,55 @@
 var canvas = document.getElementById('mapCanvas'),
-    context = canvas.getContext('2d'),
     totalTiles = 0,
-    count = 0;
+    count = 0,
+    map,
+    asset,
+    intervalId;
+
+function setupControls() {
+    var btn = document.querySelectorAll('.btn-play-pause')[0];
+    btn.addEventListener('click', function(e) {
+        e.preventDefault();
+        if (btn.innerHTML === 'play') {
+            btn.innerHTML = 'pause';
+            play();
+        } else {
+            btn.innerHTML = 'play';
+            pause();
+        }
+    });
+}
+
+var rad = 0;
+function play() {
+    if (!intervalId) {
+        intervalId = setInterval(function() {
+            map.clear();
+            map.render(rad);
+            // map.calcX = function(x, y) {
+            //     return x - (Math.sin((y + rad)/2) * 7);
+            // };
+            map.calcY = function(x, y) {
+                return y + (Math.cos((x + rad)/2) * 7);
+            };
+            rad += 1.5;
+        }, 70);
+    }
+}
+
+function pause() {
+    clearInterval(intervalId);
+    intervalId = null;
+}
 
 $.getJSON('./resources/data/assets-01.json', function(assetsData) {
     $.getJSON('./resources/data/map-01.json', function(mapData) {
 
-        var map = new GamePoc.Map(mapData, assetsData, context),
-            asset = new GamePoc.Asset(assetsData);
+        map = new GamePoc.Map(mapData, assetsData, canvas);
+        asset = new GamePoc.Asset(assetsData);
 
         asset.loadImages(function() {
-            map.render();
+            setupControls();
+            play();
         });
 
     });
--- resources/js/map.js
+++ resources/js/map.js
@@ -1,15 +1,56 @@
 var GamePoc = GamePoc || {};
 GamePoc.Map = (function() {
 
-    function Map(data, assets, context) {
+    function Map(data, assets, canvas) {
+        /* PRIVATE PROPERTIES */
+
+        var dx = Math.floor(assets.tile.width / 2),
+            dy = Math.floor(assets.tile.height / 2),
+            dz = assets.tile.depth;
+
+        /* PUBLIC PROPERTIES */
+
         this.data = data;
         this.assets = assets;
-        this.context = context;
+        this.canvas = canvas;
+        this.context = this.canvas.getContext('2d');
         this.size = {
-            width: data[0][0].length,
-            height: data[0].length
+            width: data[0].xy[0].length, // TODO Size can't be defined by data length(must be passed as a constructor parameter)
+            height: data[0].xy.length
         };
+
+        this.calcX = function(x, y) {
+            return x;
+        }
+
+        this.calcY = function(x, y) {
+            return y;
+        }
+
+        /* PUBLIC METHODS */
+
+        Map.prototype.getDeltas = function() {
+            return {dx: dx, dy:dy, dz:dz};
+        }
     }
+
+    /* PRIVATE METHODS */
+
+    function drawTile(x, y, z, key) {
+        var isoPoint = this.topDownToIso({x: x, y: y}),
+            // paddingTop = (this.data.length - 1) * this.assets.tile.depth,
+            // paddingTop = this.getMaxZ() * this.assets.tile.depth / 2,
+            paddingTop = this.assets.tile.depth,
+            deltas = this.getDeltas(),
+            xTile = isoPoint.x * deltas.dx + (this.size.width * deltas.dx),
+            yTile = isoPoint.y * deltas.dy - (z * deltas.dz) + paddingTop;
+
+        if (this.assets.images[key]) {
+            this.context.drawImage(this.assets.images[key].image, this.calcX(xTile, yTile), this.calcY(xTile, yTile));
+        }
+    }
+
+    /* PUBLIC METHODS */
 
     Map.prototype.topDownToIso = function(coord) {
         return {
@@ -18,25 +59,48 @@ GamePoc.Map = (function() {
         };
     }
 
+    Map.prototype.getData = function() {
+        return this.data;
+    }
+
+    Map.prototype.setData = function(data) {
+        this.data = data;
+    }
+
+    Map.prototype.getTile = function(x, y, z) {
+        return this.data[z][y][x];
+    }
+
+    Map.prototype.setTile = function(x, y, z, key) {
+        var tileLine = this.data[z][y].split('');
+        tileLine[x] = key;
+        this.data[z][y] = tileLine.join('');
+    }
+
+    Map.prototype.getMaxZ = function() {
+        var layer,
+            maxZ = 0;
+        for (layer = 0; layer < this.data.length; layer++) {
+            if (this.data[layer].z > maxZ) {
+                maxZ = this.data[layer].z;
+            }
+        }
+        return maxZ;
+    }
+
+    Map.prototype.clear = function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+
     Map.prototype.render = function() {
-        var dx = Math.floor(this.assets.tile.width / 2),
-            dy = Math.floor(this.assets.tile.height / 2),
-            dz = 0;
+        var layer, y, x;
 
-        for (z = 0; z < this.data.length; z++) {
-            for (y = 0; this.data[z].length && y < this.size.height; y++) { 
-                for (x = 0; x < this.size.width; x++) {
-                    var coordIso = this.topDownToIso({x: x, y: y}),
-                        paddingTop = (this.data.length - 1) * this.assets.tile.depth,
-                        xTile = coordIso.x * dx + (this.size.width * dx),
-                        yTile = coordIso.y * dy - dz + paddingTop;
-
-                        if (this.assets.images[this.data[z][y][x]]) {
-                            this.context.drawImage(this.assets.images[this.data[z][y][x]].image, xTile, yTile);
-                        }
+        for (layer = 0; layer < this.data.length; layer++) {
+            for (y = 0; y < this.data[layer].xy.length; y++) { 
+                for (x = 0; x < this.data[layer].xy[y].length; x++) {
+                    drawTile.call(this, x, y, this.data[layer].z, this.data[layer].xy[y][x]);
                 }
             }
-            dz += this.assets.tile.depth;
         }
     }
 
